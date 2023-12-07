7b4ca7a4e659859689d05afb4c3ce6556d3acda8
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2023-12-07 15:43:41 +0300

    added adaptive to header and footer

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let header_mobile = document.querySelector("#header_mobile")
+let header__mobile__close = document.querySelector("#header__mobile__close")
+let header__mobile__hamburger = document.querySelector("#header__mobile__hamburger")
+
+header__mobile__hamburger.onclick = () => {
+    header_mobile.classList.toggle("header__mobile-open");
+};
+
+header__mobile__close.onclick = () => {
+    header_mobile.classList.remove("header__mobile-open");
+};
+
+
