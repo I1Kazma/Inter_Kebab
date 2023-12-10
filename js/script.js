29dab8964daa45c57af7b5e7049c9e1f7af5b6df
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2023-12-10 16:05:34 +0300

    added top-banners with food

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ let header__mobile__hamburger = document.querySelector("#header__mobile__hamburg
 
 header__mobile__hamburger.onclick = () => {
     header_mobile.classList.toggle("header__mobile-open");
+    document.body.classList.toggle("nav--opened")
 };
 
 header__mobile__close.onclick = () => {
     header_mobile.classList.remove("header__mobile-open");
+    document.body.classList.remove("nav--opened")
 };
 
 
